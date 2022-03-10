50d688be536fa654ca73fbc811fd770b714c2c69
Dennis de Houx <dennis@aio.be>
2022-03-10 16:49:33 +0100

    Adding docblocks

--- app/Traits/BelongsToProduct.php
+++ app/Traits/BelongsToProduct.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * One For The Code - Default Laravel Traits.
+ *
+ * Commonly used traits for laravel 8.x and 9.x used by
+ * One For The Code and All In One
+ *
+ * @copyright 2022 One For The Code
+ * @author Dennis de Houx <dennis@aio.be>
+ *
+ * @category Traits
+ *
+ * @version 1.0.0
+ *
+ * @since 1.0.0
+ */
+
 namespace App\Traits;
 
 trait BelongsToProduct
